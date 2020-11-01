9e693c10a28625c6274d0f94da32dac7f08a21a2
Jake Vits <abelhla1999@gmail.com>
2020-11-01 12:19:04 +0700

    Java program from SvenGates

---
+++ Github.java
@@ -0,0 +1,18 @@
+public class Github{
+
+	String name = "Pi Pi";
+	int age = 20;
+	
+	public void introduce(){
+	
+		System.out.print("I am: " + this.name);
+		System.out.print("I am: " + this.age + " years old!");
+	}
+
+	public static void main(String []args){
+	
+		Github g = new Github();
+		g.introduce;
+	}
+
+}
